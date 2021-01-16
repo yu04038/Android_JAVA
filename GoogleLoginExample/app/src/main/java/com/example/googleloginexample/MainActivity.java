94e6e7125133789caebb494366c3a2e9ee736db5
yu04038 <69415375+yu04038@users.noreply.github.com>
2021-01-17 04:49:25 +0900

    second modified
    logout, login only for ajou mail

--- GoogleLoginExample/app/src/main/java/com/example/googleloginexample/MainActivity.java
+++ GoogleLoginExample/app/src/main/java/com/example/googleloginexample/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.googleloginexample;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -17,6 +23,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -27,6 +35,7 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private SignInButton btn_google;
+    private Button btn_logout;
     private FirebaseAuth auth; //파이어베이스 인증 객체
     private GoogleApiClient googleApiClient;
     private static final int REQ_SIGN_GOOGLE = 100; //구글로그인 했을 때 결과 코드
@@ -55,6 +64,70 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 startActivityForResult(intent, REQ_SIGN_GOOGLE);
             }
         });
+
+        btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alt_bld = new AlertDialog.Builder(v.getContext());
+                alt_bld.setMessage("로그아웃 하시겠습니까?").setCancelable(false)
+                        .setPositiveButton("네",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // 네 클릭
+                                        // 로그아웃 함수 call
+                                        signOut();
+                                    }
+                                }).setNegativeButton("아니오",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // 아니오 클릭. dialog 닫기.
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = alt_bld.create();
+                // 대화창 클릭시 뒷 배경 어두워지는 것 막기
+                //alert.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                // 대화창 제목 설정
+                alert.setTitle("로그아웃");
+                // 대화창 아이콘 설정
+                alert.setIcon(R.drawable.common_google_signin_btn_icon_dark);
+                // 대화창 배경 색 설정
+                alert.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(255,62,79,92)));
+                alert.show();
+            }
+        });
+    }
+
+    private void signOut() {
+        googleApiClient.connect();
+        googleApiClient.registerConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(@Nullable Bundle bundle) {
+                auth.signOut();
+                if (googleApiClient.isConnected()) {
+                    Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(@NonNull Status status) {
+                            if (status.isSuccess()) {
+                                Log.v("알림", "로그아웃 성공");
+                                setResult(1);
+                            } else {
+                                setResult(0);
+                            }
+                            //finish();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onConnectionSuspended(int i) {
+                Log.v("알림", "Google API Client Connection Suspended");
+                setResult(-1);
+                finish();
+            }
+        });
     }
 
     @Override
@@ -77,11 +150,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) { //로그인이 성공했다면
-                            Toast.makeText(MainActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-                            intent.putExtra("nickName", account.getDisplayName());
-                            intent.putExtra("photoUrl", String.valueOf(account.getPhotoUrl())); //특정 자료형을 string형태로 전환
-                            startActivity(intent);
+                            String email = account.getEmail().substring(account.getEmail().lastIndexOf("@"));
+                            Log.d("test", email);
+                            if (email.equals("@ajou.ac.kr")) {
+                                Toast.makeText(MainActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                                intent.putExtra("nickName", account.getDisplayName());
+                                intent.putExtra("photoUrl", String.valueOf(account.getPhotoUrl())); //특정 자료형을 string형태로 전환
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(MainActivity.this, "아주대학교 계정이 아닙니다.", Toast.LENGTH_SHORT).show();
+                                signOut();
+                            }
                         } else { //로그인이 실패했다면
                             Toast.makeText(MainActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
                         }
