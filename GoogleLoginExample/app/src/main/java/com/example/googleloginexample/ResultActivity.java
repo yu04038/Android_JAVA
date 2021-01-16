01ae2bdcb029572ed8d6af6a85c294517eef13ac
yu04038 <69415375+yu04038@users.noreply.github.com>
2021-01-16 23:55:50 +0900

    Upload codes

---
+++ GoogleLoginExample/app/src/main/java/com/example/googleloginexample/ResultActivity.java
@@ -0,0 +1,33 @@
+package com.example.googleloginexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private TextView tv_result;
+    private ImageView iv_profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String nickName = intent.getStringExtra("nickName");
+        String photoUrl = intent.getStringExtra("photoUrl");
+
+        tv_result = findViewById(R.id.tv_result);
+        tv_result.setText(nickName);
+
+        iv_profile = findViewById(R.id.iv_profile);
+        Glide.with(this).load(photoUrl).into(iv_profile); //프로필 url을 이미지 뷰에 저장
+
+    }
+}
