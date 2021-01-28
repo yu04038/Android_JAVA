b8a8af878c5c0fa1280e34a62ae27b134bcb889c
yu04038 <69415375+yu04038@users.noreply.github.com>
2021-01-29 02:04:12 +0900

    전필, 전선, 교양 추가, animation 추가

--- PieChartExample/app/src/main/java/com/example/piechartexample/MainActivity.java
+++ PieChartExample/app/src/main/java/com/example/piechartexample/MainActivity.java
@@ -1,23 +1,66 @@
 package com.example.piechartexample;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+
 import com.dinuscxj.progressbar.CircleProgressBar;
 
 public class MainActivity extends AppCompatActivity implements CircleProgressBar.ProgressFormatter {
 
     private static final String DEFAULT_PATTERN = "%d%%";
 
-    CircleProgressBar circleProgressBar;
+    CircleProgressBar circleProgressBar1, circleProgressBar2, circleProgressBar3;
+    int i, j, k = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        circleProgressBar = findViewById(R.id.cpb_circlebar);
+        circleProgressBar1 = findViewById(R.id.cpb_circlebar1);
+        circleProgressBar2 = findViewById(R.id.cpb_circlebar2);
+        circleProgressBar3 = findViewById(R.id.cpb_circlebar3);
 
-        circleProgressBar.setProgress(70);  // 해당 퍼센트를 적용
+        final Handler handler1 = new Handler();
+        final Handler handler2 = new Handler();
+        final Handler handler3 = new Handler();
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (i <= 70) {
+                    circleProgressBar1.setProgress(i);
+                    i++;
+                    handler1.postDelayed(this, 10);
+                } else {
+                    handler1.removeCallbacks(this);
+                }
+            }
+        }, 10);
+        handler2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (j <= 50) {
+                    circleProgressBar2.setProgress(j);
+                    j++;
+                    handler2.postDelayed(this, 10);
+                } else {
+                    handler2.removeCallbacks(this);
+                }
+            }
+        }, 10);
 
+        handler3.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (k <= 90) {
+                    circleProgressBar3.setProgress(k);
+                    k++;
+                    handler3.postDelayed(this, 10);
+                } else {
+                    handler3.removeCallbacks(this);
+                }
+            }
+        }, 10);
     }// onCreate()..
 
     @Override
