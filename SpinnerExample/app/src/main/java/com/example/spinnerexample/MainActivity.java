af42cf51fc4ae315b96eca61c3c2fe2b17b3541e
Yongjin Jo <69415375+yu04038@users.noreply.github.com>
2021-02-08 23:56:20 +0900

    Upload Spinner Example

---
+++ SpinnerExample/app/src/main/java/com/example/spinnerexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.spinnerexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.tv_textview);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText("아이템 : " + parent.getItemAtPosition(position));
+            }
+            
+        });
+    }
+}
